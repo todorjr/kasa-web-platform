e3e6f77b9e56c07bee5d0bc9841f345d91ae0798
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-08-24 11:24:48 +0200

    router place changed

---
+++ src/Router/index.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Details from '../pages/Details';
+import Error from '../pages/Error';
+import { createBrowserRouter } from 'react-router-dom';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/:id',
+    element: <Details />,
+  },
+  {
+    path: '*',
+    element: <Error />,
+  },
+]);
+
+
--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './pages/Home';
-import About from './pages/About';
-import Details from './pages/Details';
-import Error from './pages/Error';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import ReactDOM from 'react-dom';
+import { router } from '../src/Router/index.js';
+import { RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/:id" element={<Details />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
