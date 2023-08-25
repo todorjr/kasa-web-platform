b26d10a8416324c547a05835a7f16c9102eb6d48
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-08-25 12:09:35 +0200

    console error fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { router } from './Router/index.js';
 import { RouterProvider } from 'react-router-dom';
 
