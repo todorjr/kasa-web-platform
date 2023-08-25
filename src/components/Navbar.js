6158f88ab8065842b168679af980824c596d1359
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-08-25 14:31:50 +0200

    useLocation to have underline style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from '../styles/Navbar.module.css';
 import logoSrc from '../assets/LOGO.jpg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 
 function Navbar() {
+
+    const [currentPage, setCurrentPage] = useState(false);
+    const location = useLocation(); // Get the current location using react-router's useLocation hook
+
+    useEffect(() => {
+        setCurrentPage(location.pathname);
+    }, [location.pathname]);
+
     return (
         <div className={styles.navbar}>
             <div className={styles.logo}>
                 <Link to='/'><img src={logoSrc} alt="Logo" /></Link>
             </div>
             <div className={styles.links}>
-                <Link to="/" className={styles.link}>Accueil</Link>
-                <Link to="/about" className={styles.link}>A propos</Link>
+                <Link to="/" className={`${styles.link} ${currentPage === '/' ? styles.active : ''}`}>Accueil</Link>
+                <Link to="/about" className={`${styles.link} ${currentPage === '/about' ? styles.active : ''}`}>A propos</Link>
             </div>
         </div>
     )
