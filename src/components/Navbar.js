30635234b3f06fa51a921e5b4c35eecb3bb4e2ce
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-09-01 13:49:20 +0200

    navigation using nav link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from '../styles/Navbar.module.css';
 import logoSrc from '../assets/LOGO.jpg';
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 function Navbar() {
-
-    const [currentPage, setCurrentPage] = useState(false);
-    const location = useLocation(); // Get the current location using react-router's useLocation hook
-
-    useEffect(() => {
-        setCurrentPage(location.pathname);
-    }, [location.pathname]);
-
     return (
         <div className={styles.navbar}>
             <div className={styles.logo}>
-                <Link to='/'><img src={logoSrc} alt="Logo" /></Link>
+                <NavLink to='/'><img src={logoSrc} alt="Logo" /></NavLink>
             </div>
-            <div className={styles.links}>
-                <Link to="/" className={`${styles.link} ${currentPage === '/' ? styles.active : ''}`}>Accueil</Link>
-                <Link to="/about" className={`${styles.link} ${currentPage === '/about' ? styles.active : ''}`}>A propos</Link>
-            </div>
+            <nav className={styles.links}>
+                <NavLink style={({ isActive }) => { return { textDecoration: isActive ? "underline" : "none",}}} to="/">Accueil</NavLink>
+                <NavLink style={({ isActive }) => { return { textDecoration: isActive ? "underline" : "none",}}} to="/about">A propos</NavLink>
+            </nav>
         </div>
     )
 }
 export default Navbar;
+
+
